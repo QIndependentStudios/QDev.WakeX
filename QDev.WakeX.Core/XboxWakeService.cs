a317654fed1e47343daf0218c36e130eb54a722e
Quoc-Thy Ngo <qngo@live.com>
2018-02-18 21:47:02 -0500

    Fixed incorrect bytes in packett.

--- QDev.WakeX.Core/XboxWakeService.cs
+++ QDev.WakeX.Core/XboxWakeService.cs
@@ -10,38 +10,53 @@ namespace QDev.WakeX.Core
 {
     public class XboxWakeService
     {
-        public void Wake()
+        private const string PingPacket = "DD00000A000000000000000400000002";
+        private const int Port = 5050;
+        private const int RetryDelay = 100;
+
+        public void Wake(string ipAddress = "10.0.0.9", string xboxLiveID = "FD00A8E459035C35")
         {
-            var xboxLiveID = "FD00A8E459035C35";
-            using (var socket = new Socket(SocketType.Dgram, ProtocolType.Udp))
+            for (int i = 0; i < 200; i++)
             {
-                socket.Blocking = false;
-                //socket.Bind(new IPEndPoint(IPAddress.Parse(""), 0));
-                socket.Connect(new IPEndPoint(IPAddress.Parse("10.0.0.9"), 5050));
-
-
-                var payload = new List<byte>();
-                payload.Add(0x00);
-                payload.AddRange(Encoding.UTF8.GetBytes(xboxLiveID.Length.ToString()));
-                payload.AddRange(Encoding.UTF8.GetBytes(xboxLiveID));
-                payload.Add(0x00);
-
-                var header = new List<byte>();
-                header.Add(0xdd);
-                header.Add(0x02);
-                header.Add(0x00);
-                header.AddRange(Encoding.UTF8.GetBytes(payload.Count.ToString()));
-                header.Add(0x00);
-                header.Add(0x00);
-
-                var packet = header.Concat(payload).ToList();
-
-                for (int i = 0; i < 10; i++)
+                using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
                 {
+                    socket.Blocking = false;
+                    socket.Connect(new IPEndPoint(IPAddress.Parse(ipAddress), Port));
+
+
+                    var payload = new List<byte>
+                    {
+                        0x00,
+                        BitConverter.GetBytes((char)xboxLiveID.Length)[0]
+                    };
+                    payload.AddRange(Encoding.UTF8.GetBytes(xboxLiveID));
+                    payload.Add(0x00);
+
+                    var header = new List<byte>
+                    {
+                        0xdd,
+                        0x02,
+                        0x00,
+                        BitConverter.GetBytes((char)payload.Count)[0],
+                        0x00,
+                        0x00
+                    };
+
+                    var packet = header.Concat(payload).ToList();
+                    Console.WriteLine($"Attempt {i}. Sending {BitConverter.ToString(packet.ToArray())}");
                     socket.Send(packet.ToArray());
-                    Thread.Sleep(new TimeSpan(0, 0, 1));
+                    Thread.Sleep(RetryDelay);
                 }
             }
+        }
+
+        public static byte[] StringToByteArray(String hex)
+        {
+            int NumberChars = hex.Length;
+            byte[] bytes = new byte[NumberChars / 2];
+            for (int i = 0; i < NumberChars; i += 2)
+                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            return bytes;
         }
     }
 }
