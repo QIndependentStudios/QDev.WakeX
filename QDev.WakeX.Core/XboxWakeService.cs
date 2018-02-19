022b479f279275fb28ee0ab6a6312d28775268f5
Quoc-Thy Ngo <qngo@live.com>
2018-02-18 20:04:55 -0500

    Add initial port of python code.

---
+++ QDev.WakeX.Core/XboxWakeService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace QDev.WakeX.Core
+{
+    public class XboxWakeService
+    {
+        public void Wake()
+        {
+            var xboxLiveID = "FD00A8E459035C35";
+            using (var socket = new Socket(SocketType.Dgram, ProtocolType.Udp))
+            {
+                socket.Blocking = false;
+                //socket.Bind(new IPEndPoint(IPAddress.Parse(""), 0));
+                socket.Connect(new IPEndPoint(IPAddress.Parse("10.0.0.9"), 5050));
+
+
+                var payload = new List<byte>();
+                payload.Add(0x00);
+                payload.AddRange(Encoding.UTF8.GetBytes(xboxLiveID.Length.ToString()));
+                payload.AddRange(Encoding.UTF8.GetBytes(xboxLiveID));
+                payload.Add(0x00);
+
+                var header = new List<byte>();
+                header.Add(0xdd);
+                header.Add(0x02);
+                header.Add(0x00);
+                header.AddRange(Encoding.UTF8.GetBytes(payload.Count.ToString()));
+                header.Add(0x00);
+                header.Add(0x00);
+
+                var packet = header.Concat(payload).ToList();
+
+                for (int i = 0; i < 10; i++)
+                {
+                    socket.Send(packet.ToArray());
+                    Thread.Sleep(new TimeSpan(0, 0, 1));
+                }
+            }
+        }
+    }
+}
---
+++ QDev.WakeX.TestUtil/Program.cs
@@ -0,0 +1,13 @@
+﻿using QDev.WakeX.Core;
+
+namespace QDev.WakeX.TestUtil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wakeService = new XboxWakeService();
+            wakeService.Wake();
+        }
+    }
+}
